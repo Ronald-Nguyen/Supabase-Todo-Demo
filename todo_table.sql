fd86c99e4b4f85551b810929ca49070d1bd3792d
Ronald Nguyen <ronaldnguyen314@gmail.com>
2026-04-18 23:53:41 +0200

    update sql table

--- todo_table.sql
+++ todo_table.sql
@@ -1,35 +1,39 @@
-create table public.todos (
+create table if not exists public.todos (
   id bigint generated always as identity primary key,
   text text not null,
-  created_at timestamp with time zone default now()
+  created_at timestamp with time zone default now(),
+  user_id uuid references auth.users(id) not null
 );
 
-alter table public.todos
-add column user_id uuid references auth.users(id);
+alter table public.todos enable row level security;
 
+drop policy if exists "users can read own todos" on public.todos;
 create policy "users can read own todos"
 on public.todos
 for select
 to authenticated
-using ((select auth.uid()) = user_id);
+using (auth.uid() = user_id);
 
+drop policy if exists "users can insert own todos" on public.todos;
 create policy "users can insert own todos"
 on public.todos
 for insert
 to authenticated
-with check ((select auth.uid()) = user_id);
+with check (auth.uid() = user_id);
 
+drop policy if exists "users can delete own todos" on public.todos;
 create policy "users can delete own todos"
 on public.todos
 for delete
 to authenticated
-using ((select auth.uid()) = user_id);
+using (auth.uid() = user_id);
 
-alter publication supabase_realtime add table public.todos;
-
+drop policy if exists "users can update own todos" on public.todos;
 create policy "users can update own todos"
 on public.todos
 for update
 to authenticated
 using (auth.uid() = user_id)
 with check (auth.uid() = user_id);
+
+alter publication supabase_realtime add table public.todos;
